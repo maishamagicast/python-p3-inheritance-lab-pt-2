6481729342ada5ebfddff9026d6b601ec4407365
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-06-19 17:16:30 +0300

    Class inheritance

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,21 @@
 class Student:
-    pass
+    def __init__(self):
+        pass
+    
+    def hello(self):
+        print( "Hey there! I'm so excited to learn stuff.")
+        
+    def raise_hand(self):
+        print('Pick me!')
 
 class ChattyStudent(Student):
-    pass
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        for i in range(10):
+            print('Pick me!')
